c552210d1a3c006e904841b6b7bb645d8a52b76f
Leen-E05 <leenabusalem1@gmail.com>
2025-10-25 14:46:57 +0300

    Project cleanup: add page routes, fix CSS and JS links

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import joblib
 import pandas as pd
@@ -10,6 +10,18 @@ CORS(app)  # Enable CORS for all routes
 # Load models 
 reg_model = joblib.load("models/regression_model.joblib")
 clf_model = joblib.load("models/classification_model.joblib")
+
+@app.route("/")
+def home():
+    return render_template("homePage.html")  
+
+@app.route("/hospital")
+def hospital():
+    return render_template("Hospital.html")
+
+@app.route("/patient")
+def patient():
+    return render_template("Patient.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
